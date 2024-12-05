fc562ae8cbe2d46303254fab21f56e98abc78e92
Nil Serra Tió <nilserratio@gmail.com>
2024-12-05 23:20:43 +0100

    Create TableComponent component with the table component headers

---
+++ components/TableComponent/TableComponent.js
@@ -0,0 +1,140 @@
+import React from "react";
+import tableComponentStyles from "./TableComponentStyles";
+import portfolioStyles from "../Portfolio/PortfolioStyles";
+
+const TableComponent = () => {
+  const headers = [
+    {
+      id: "1",
+      name: "Rank",
+      class: "rank",
+      tooltipText:
+        "Ranking based on AI Score, the lower the ranking, the better",
+    },
+    { id: "2", name: "Company", class: "company", tooltipText: "Company name" },
+    {
+      id: "3",
+      name: "Country",
+      class: "country",
+      tooltipText: "Country where the headquarters are located",
+    },
+    {
+      id: "4",
+      name: "AI Score",
+      class: "scoretable",
+      tooltipText:
+        "Danelfin AI global score based on all data available (1-10)",
+    },
+    {
+      id: "5",
+      name: "Change",
+      class: "change",
+      tooltipText: "Change in AI Score vs the previous day",
+    },
+    {
+      id: "6",
+      name: "Fundamental",
+      class: "fundamental",
+      tooltipText:
+        "Danelfin AI subscore only based on company fundamental indicators (1-10)",
+    },
+    {
+      id: "7",
+      name: "Teachnical",
+      class: "technical",
+      tooltipText:
+        "Danelfin AI subscore only based on technical indicators produced by price & volume (1-10)",
+    },
+    {
+      id: "8",
+      name: "Sentiment",
+      class: "sentiment",
+      tooltipText:
+        "Danelfin AI subscore only based on sentiment indicators (1-10)",
+    },
+    {
+      id: "9",
+      name: "Low Risc",
+      class: "risk",
+      tooltipText:
+        "Risk subscore based on the negative price fluctuations (semi-deviation) latest 500 market days. The higher the score, the lower the downside risk.",
+    },
+  ];
+
+  return (
+    <>
+      <div>
+        <div className="table-component-container table-component-footer-border-radius">
+          <div className="table-component-content-wrapper">
+            <span className="table-component-alpha-col-dropdown-mobile"></span>
+            <div className="table-component-content">
+              <div className="table-component-shadow-background"></div>
+              <div className="table-container table-component-with-alpha-column">
+                <div className="table-component-header">
+                  <div className="table-component-with-alpha-column table-component-row justify-between">
+                    {headers.map((header) =>
+                      header.class !== "scoretable" ? (
+                        <div
+                          key={header.id}
+                          className={`table-component-column-${header.class} table-component-data-head tooltipCustomStockList tootipCustomStockLlistDoubleSize`}
+                        >
+                          <span>{header.name}</span>
+                          <span className="tooltiptextCustomStockList">
+                            {header.tooltipText}
+                          </span>
+                        </div>
+                      ) : (
+                        <div
+                          key={header.id}
+                          className={`table-component-column-score table-component-data-head tooltipCustomStockList tootipCustomStockLlistDoubleSize  table-component-column-score-small`}
+                        >
+                          <span>
+                            <span>{header.name}</span>
+                            <img
+                              src="https://cdn.danelfin.com/assets/next/images/icons/ico-info-white.svg"
+                              alt="info icon"
+                            />
+                          </span>
+                          <span className="tooltiptextCustomStockList">
+                            {header.tooltipText}
+                          </span>
+                        </div>
+                      )
+                    )}
+                    <div className="table-component-alpha-col table-component-data-head">
+                      <span className="table-component-alpha-col-content">
+                        <div className="table-component-alpha-col-title">
+                          Perf YTD
+                        </div>
+                        <div className="table-component-alpha-col-image-container">
+                          <img
+                            src="https://cdn.danelfin.com/assets/next/images/icons/v1Dropdown.svg"
+                            alt="check icon"
+                            className="table-component-alpha-col-image"
+                          />
+                          <img
+                            src="https://cdn.danelfin.com/assets/next/images/icons/v1DropdownHover.svg"
+                            alt="check icon"
+                            className="table-component-alpha-col-image-hover"
+                          />
+                        </div>
+                      </span>
+                    </div>
+                    <div className="table-component-column-country-mobile table-component-data-head">
+                      <span>Country</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="table-component-body"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <style jsx>{tableComponentStyles}</style>
+      <style jsx>{portfolioStyles}</style>
+    </>
+  );
+};
+
+export default TableComponent;
