b7c51e6528de7eda495ae4d7d9514c3810fd3d07
Nil Serra Tió <nilserratio@gmail.com>
2024-12-06 09:37:00 +0100

    Change useStocks custom hook to fit the API endpoint

--- hooks/useStocks/useStocks.tsx
+++ hooks/useStocks/useStocks.tsx
@@ -1,33 +1,40 @@
 import axios from "axios";
 import { useCallback } from "react";
-import { Stock, StocksResponse } from "./types";
+import { StocksResponse } from "./types";
 
-const apiUrl = "/api/v1/datasets";
+interface GetStocksParams {
+  limit?: number;
+  offset?: number;
+  order_by?: string;
+  order_dir?: "asc" | "desc";
+  isETF?: boolean;
+  country?: string;
+}
+
+const apiUrl = "http://localhost:5000/stocks";
 
 const useStocks = () => {
-  const getStocks = useCallback(async (): Promise<StocksResponse[] | null> => {
-    try {
-      const response = await axios.get<{ stocks: StocksResponse[] }>(
-        `${apiUrl}`,
-        {
+  const getStocks = useCallback(
+    async (params: GetStocksParams = {}): Promise<StocksResponse | null> => {
+      try {
+        const response = await axios.get<StocksResponse>(apiUrl, {
+          params: {
+            ...params,
+          },
           headers: {
             "Content-Type": "application/json",
             "Cache-Control": "max-age=86400",
           },
-        }
-      );
+        });
 
-      if (response.data.stocks.length === 0) {
-        console.error("No stocks found");
+        return response.data;
+      } catch (error) {
+        console.error("Couldn't load stocks:", error);
         return null;
       }
-
-      return response.data.stocks;
-    } catch (error) {
-      console.error("Couldn't load stocks");
-      return null;
-    }
-  }, []);
+    },
+    []
+  );
 
   return { getStocks };
 };
